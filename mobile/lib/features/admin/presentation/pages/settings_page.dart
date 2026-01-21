e917b91756d002daa0a97717b0552d81b6457671
hkm <mehdihakkoum@gmail.com>
2026-01-21 11:55:12 +0100

    Fix missing material import in settings_page.dart

--- mobile/lib/features/admin/presentation/pages/settings_page.dart
+++ mobile/lib/features/admin/presentation/pages/settings_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../../../core/services/settings_service.dart';
 import '../../../../core/providers/theme_provider.dart';
