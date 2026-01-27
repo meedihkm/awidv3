3f5a98d6a57d7600aef37edf8621a31a2a19974d
Mehdi Hakkoum <mehdihakkoum@gmail.com>
2026-01-27 05:04:55 +0100

    chore: remove api-v2 and mobile folders from repo (keep local only)

--- api-v2/queues/email.queue.js
+++
@@ -1,9 +0,0 @@
-const { Queue } = require('bullmq');
-const { connection, defaultJobOptions } = require('../config/queue.config');
-
-const emailQueue = new Queue('email', {
-    connection,
-    defaultJobOptions,
-});
-
-module.exports = emailQueue;
--- api-v2/queues/report.queue.js
+++
@@ -1,9 +0,0 @@
-const { Queue } = require('bullmq');
-const { connection, defaultJobOptions } = require('../config/queue.config');
-
-const reportQueue = new Queue('report', {
-    connection,
-    defaultJobOptions,
-});
-
-module.exports = reportQueue;
